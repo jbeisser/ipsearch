153f43318f903b473c1f9394f4601a7d436a228b
johan beisser <johan.beisser@flashtalking.com>
2022-03-16 20:19:46 -0700

    Initial commit.

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"ipsearch"
 )
 
 func main() {
-	// get IP from stdin
-	ip := os.Args[1]
-	f := ipsearch.InternetDBLookup(ip)
-	fmt.Println(f.Ip)
-	for p := range f.Ports {
-		fmt.Println(f.Ports[p])
-	}
+	// get IP from argv
+	//ip := os.Args[1]
+	reader := bufio.NewReader(os.Stdin)
+	s, _ := reader.ReadString('\n')
 
+	s = strings.TrimSuffix(s, "\n")
+	ips := strings.Split(s, " ")
+
+	for p := range ips {
+		f := ipsearch.InternetDBLookup(ips[p])
+		fmt.Printf("IP: %s\n", f.Ip)
+
+		for p := range f.Ports {
+			fmt.Println(f.Ports[p])
+		}
+	}
 }
